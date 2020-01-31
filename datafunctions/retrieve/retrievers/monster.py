fd5aadef59c08a5369cef44b0771b172898dbd4c
larkeith <llarkeith@gmail.com>
2020-01-31 15:19:45 -0800

    Update monster scraper to add to AWS

--- datafunctions/retrieve/retrievers/monster.py
+++ datafunctions/retrieve/retrievers/monster.py
@@ -6,6 +6,9 @@ import logging
 import time
 import psycopg2
 import bs4
+import random
+
+from decouple import config
 
 from urllib.parse import urlencode
 from selenium import webdriver
@@ -103,7 +106,7 @@ class MonsterScraper:  # (DataRetriever):
 				WHERE title = %(title)s
 			), descriptions AS (
 				SELECT job_id
-				FROM jobs_descriptions
+				FROM job_descriptions
 				WHERE description = %(description)s
 			)
 			SELECT listings.id
@@ -113,15 +116,39 @@ class MonsterScraper:  # (DataRetriever):
 			LIMIT 1;
 		"""
 
+		job_exists_query_2 = """
+			WITH listings AS (
+				SELECT id
+				FROM job_listings
+				WHERE title = %(title)s
+			), c AS (
+				SELECT id
+				FROM companies
+				WHERE name = %(name)s
+			)
+			SELECT listings.id
+			FROM listings
+			INNER JOIN job_companies
+			ON job_companies.job_id = listings.id
+			INNER JOIN c
+			ON job_companies.company_id = c.id
+			LIMIT 1
+		"""
+
 		job_listings_query = """
 			INSERT INTO job_listings(title, post_date_utc)
 			VALUES (%(title)s, to_timestamp(%(post_date_utc)s))
 			RETURNING id;
 		"""
 
-		jobs_descriptions_query = """
-			INSERT INTO jobs_descriptions(job_id, description)
+		job_descriptions_query = """
+			INSERT INTO job_descriptions(job_id, description)
 			VALUES (%(job_id)s, %(description)s);
+		"""
+
+		job_links_query = """
+			INSERT INTO job_links(job_id, external_url)
+			VALUES (%(job_id)s, %(external_url)s);
 		"""
 
 		company_exists_query = """
@@ -137,8 +164,8 @@ class MonsterScraper:  # (DataRetriever):
 			RETURNING id;
 		"""
 
-		jobs_companies_query = """
-			INSERT INTO jobs_companies(job_id, company_id)
+		job_companies_query = """
+			INSERT INTO job_companies(job_id, company_id)
 			VALUES (%(job_id)s, %(company_id)s);
 		"""
 
@@ -157,12 +184,12 @@ class MonsterScraper:  # (DataRetriever):
 			RETURNING id;
 		"""
 
-		jobs_locations_query = """
-			INSERT INTO jobs_locations(job_id, location_id)
+		job_locations_query = """
+			INSERT INTO job_locations(job_id, location_id)
 			VALUES (%(job_id)s, %(location_id)s);
 		"""
 
-		# Run order: job_listings, jobs_descriptions, companies, jobs_companies
+		# Run order: job_listings, job_descriptions, companies, job_companies
 		curr = db_conn.cursor()
 
 		# Get the company id if it exists
@@ -196,14 +223,6 @@ class MonsterScraper:  # (DataRetriever):
 				'country': result['country'],
 			}
 		)
-		MONSTER_LOG.info(curr.mogrify(
-			location_exists_query,
-			{
-				'city': result['city'],
-				'state_province': result['state_province'],
-				'country': result['country'],
-			}
-		))
 		qr = curr.fetchone()
 		if qr is not None:
 			MONSTER_LOG.info(f'Location {result["city"]}, {result["state_province"]} already exists in DB.')
@@ -230,9 +249,21 @@ class MonsterScraper:  # (DataRetriever):
 			}
 		)
 		qr = curr.fetchone()
+		# Get the job listing id if it exists, by company
+		curr.execute(
+			job_exists_query_2,
+			{
+				'title': result['title'],
+				'name': result['company_name'],
+			}
+		)
+		qr2 = curr.fetchone()
 		if qr is not None:
 			MONSTER_LOG.info(f'Job listing for {result["title"]} already exists in DB.')
 			job_id = qr[0]
+		if qr2 is not None:
+			MONSTER_LOG.info(f'Job listing for {result["title"]} at company {result["company_name"]} already exists in DB.')
+			job_id = qr2[0]
 		else:
 			# Otherwise, insert the job listing and get the id
 			MONSTER_LOG.info(f'Job listing for {result["title"]} not yet in DB, adding...')
@@ -248,7 +279,7 @@ class MonsterScraper:  # (DataRetriever):
 			# Also add the relation to companies
 			MONSTER_LOG.info(f'Adding relation job_id {job_id} to company_id {company_id}...')
 			curr.execute(
-				jobs_companies_query,
+				job_companies_query,
 				{
 					'job_id': job_id,
 					'company_id': company_id,
@@ -258,7 +289,7 @@ class MonsterScraper:  # (DataRetriever):
 			# Also add the relation to locations
 			MONSTER_LOG.info(f'Adding relation job_id {job_id} to location_id {location_id}...')
 			curr.execute(
-				jobs_locations_query,
+				job_locations_query,
 				{
 					'job_id': job_id,
 					'location_id': location_id,
@@ -268,10 +299,20 @@ class MonsterScraper:  # (DataRetriever):
 			# And the description
 			MONSTER_LOG.info('Saving description...')
 			curr.execute(
-				jobs_descriptions_query,
+				job_descriptions_query,
 				{
 					'job_id': job_id,
 					'description': result['description'],
+				}
+			)
+
+			# And the link to the job
+			MONSTER_LOG.info('Saving link...')
+			curr.execute(
+				job_links_query,
+				{
+					'job_id': job_id,
+					'external_url': result['inner_link'],
 				}
 			)
 
@@ -312,7 +353,7 @@ class MonsterScraper:  # (DataRetriever):
 				tries = 0
 				page_count += 1
 
-				wait_time = 0
+				wait_time = 1
 				MONSTER_LOG.info(f'Loaded jobs, waiting {wait_time} seconds...')
 				time.sleep(wait_time)
 				# load_button.click()
@@ -381,7 +422,7 @@ class MonsterScraper:  # (DataRetriever):
 				result['city'] = ''  # These would more properly be None
 				result['state_province'] = ''  # However, psycopg2 manages to be utterly useless at handling NULL
 				result['zip'] = ''  # So instead we use empty strings
-				result['country'] = 'United States'  # Good job, psycopg2
+				result['country'] = ''  # Good job, psycopg2
 				try:
 					location_parts = result['location'].split(',')
 					result['city'] = location_parts[0].strip().title()
@@ -490,9 +531,11 @@ class MonsterScraper:  # (DataRetriever):
 		return super().get_data()
 
 
+title_list = ['Data Analyst', 'Web Engineer', 'Software Engineer', 'UI Engineer', 'Backend Engineer', 'Machine Learning Engineer', 'Frontend Engineer', 'Support Engineer', 'Full-stack Engineer', 'QA Engineer', 'Web Developer', 'Software Developer', 'UI Developer', 'Backend Developer', 'Machine Learning Developer', 'Frontend Developer', 'Support Developer', 'Full-stack Developer', 'QA Developer', 'Developer']
+random.shuffle(title_list)
 a = MonsterScraper()
-with psycopg2.connect(database='jobs') as psql_conn:
-	for job in ['', 'Data Analyst', 'Web Engineer', 'Software Engineer', 'UI Engineer', 'Backend Engineer', 'Machine Learning Engineer', 'Frontend Engineer', 'Support Engineer', 'Full-stack Engineer', 'QA Engineer', 'Web Developer', 'Software Developer', 'UI Developer', 'Backend Developer', 'Machine Learning Developer', 'Frontend Developer', 'Support Developer', 'Full-stack Developer', 'QA Developer', 'Developer']:
+with psycopg2.connect(dbname=config("DB_DB"), user=config("DB_USER"), password=config("DB_PASSWORD"), host=config("DB_HOST"), port=config("DB_PORT")) as psql_conn:
+	for job in title_list:
 		a.get_jobs(psql_conn, job_title=job)
 a.driver.close()
 # print(r[-1])
