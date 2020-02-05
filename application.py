a09d91537df77768fe59a594f4ca96fe605bf7a9
larkeith <llarkeith@gmail.com>
2020-02-04 16:36:58 -0800

    Add logging

--- application.py
+++ application.py
@@ -2,12 +2,23 @@
 import os
 import subprocess
 import sys
+import logging
 
 from flask import Flask, jsonify
+from flask.logging import default_handler
+from datafunctions.log.log import startLog, getLogFile
 
-application = Flask(__name__)
 
 SCRAPER_NAME = './run_scrapers.py'
+startLog(getLogFile(__file__))
+APP_LOG = logging.getLogger('root')
+
+APP_LOG.info('Creating app...')
+application = Flask(__name__)
+werkzeug_logger = logging.getLogger('werkzeug')
+for handler in APP_LOG.handlers:
+	werkzeug_logger.addHandler(handler)
+	application.logger.addHandler(handler)
 
 
 @application.route('/start')
@@ -23,13 +34,17 @@ def start():
 		'message': 'Unknown failure.'
 	}
 	try:
-		while not check_running(SCRAPER_NAME) and tries < 5:
+		APP_LOG.info('/start called')
+		max_tries = 5
+		while not check_running(SCRAPER_NAME) and tries < max_tries:
+			APP_LOG.info(f'Scraper not running, attempting to start it (try {tries + 1} of {max_tries})')
 			with open(os.devnull, 'r+b', 0) as DEVNULL:
 				subprocess.Popen(['nohup', sys.executable, SCRAPER_NAME],
 					stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL, close_fds=True, preexec_fn=os.setpgrp)
 			tries += 1
 
 		if check_running(SCRAPER_NAME):
+			APP_LOG.info(f'Scraper running.')
 			if tries == 0:
 				result = {
 					'running': True,
@@ -49,20 +64,27 @@ def start():
 				'message': f'Failed to start {SCRAPER_NAME} after {tries} tries.'
 			}
 
+		APP_LOG.info(f'result: {result}')
+
 	except Exception as e:
 		result = {
 			'running': False,
 			'tries': tries,
 			'message': f'Aborting after {type(e)} exception on try {tries}: {e}'
 		}
+		APP_LOG.info(f'result: {result}')
+		APP_LOG.info(e, exc_info=True)
 
 	return jsonify(result)
 
 
 def check_running(pname):
+	APP_LOG.info(f'check_running called, pname: {pname}')
 	result = os.system(f'ps -Af | grep -v grep | grep -v log | grep {pname}')
+	APP_LOG.info(f'exit code: {result}')
 	return result == 0
 
 
 if __name__ == '__main__':
+	APP_LOG.info('Starting Flask dev server...')
 	application.run()
