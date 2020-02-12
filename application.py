4ae753176a4eee8fa1e2ec347494dee19b5c4b7f
larkeith <llarkeith@gmail.com>
2020-02-12 08:35:03 -0800

    Update /health dstat args

--- application.py
+++ application.py
@@ -59,7 +59,7 @@ def health():
 	outputs = {}
 	outputs['scrapers running'] = check_running(SCRAPER_NAME)
 	outputs['free'] = os.popen('free -h').read()
-	outputs['dstat'] = os.popen('dstat -afv 1 0').read()
+	outputs['dstat'] = os.popen('dstat -cdlimnsty 1 0 | cat').read()
 	outputs['top'] = os.popen('top -bn1').read()
 	outputs['ps'] = os.popen('ps -Afly --forest').read()
 	APP_LOG.info(f'Health results: {outputs}')
